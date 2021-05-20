2175cb5f34ff615caeb21c34a559155a91f12660
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-05-20 12:52:30 +0530

    Add files via upload

--- models/FbUser.js
+++ models/FbUser.js
@@ -1,1 +1,24 @@
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb+srv://admin-hanamantray:Test-123@cluster0.8ric1.mongodb.net/fbuserDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify:true
+});
+
+var fbuserSchema = new mongoose.Schema({
+    uid: String,
+    email: String,
+    name: String,
+    fname: String,
+    pic: String
+});
+var guserSchema = new mongoose.Schema({
+    uid: String,
+    email: String,
+    name: String,
+    fname: String,
+    pic: String
+});
+module.exports = mongoose.model('FbUser', fbuserSchema);
+module.exports = mongoose.model('GUser',guserSchema);
